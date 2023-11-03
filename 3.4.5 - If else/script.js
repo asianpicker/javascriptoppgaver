2a533d46774c7bee53a43d7cb93af712100460dd
asianpicker <149372617+asianpicker@users.noreply.github.com>
2023-11-03 12:48:10 +0100

    did the if else task

---
+++ 3.4.5 - If else/script.js
@@ -0,0 +1,14 @@
+const out = document.getElementById("out")
+
+let x = 10
+let y = 5
+let z = 5
+let switch0n = true //boolean
+
+if (switch0n) {
+    out.textContent = "ja"
+}
+
+else {
+    out.textContent = "nei"
+}
