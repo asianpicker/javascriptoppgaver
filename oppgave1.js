dbae59c52e76fe9529fadb41fa453651684525ee
asianpicker <149372617+asianpicker@users.noreply.github.com>
2023-11-03 09:19:54 +0100

    Just did the task 1

---
+++ oppgave1.js
@@ -0,0 +1,4 @@
+// const paragraph = document.getElementById("hello")
+// viste ikke helt om jeg skulle endre navnet fra document til paragraph
+
+document.getElementById("hello").textContent="Hello World!"
